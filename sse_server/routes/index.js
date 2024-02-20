8310cb09ce415848cb8686fe196f1ae1cfd302c7
“gongjian” <15031617367@163.com>
2024-02-20 21:01:48 +0800

    实现sse请求

--- sse_server/routes/index.js
+++ sse_server/routes/index.js
@@ -1,19 +1,42 @@
 const router = require('koa-router')()
+const { PassThrough } = require("stream");
 
-router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
+router.get('/', async (ctx, next) => {  
+  ctx.request.socket.setTimeout(0);  
+  ctx.req.socket.setNoDelay(true);  
+  ctx.req.socket.setKeepAlive(true);  
+  ctx.set({  
+    "Content-Type": "text/event-stream",  
+    "Cache-Control": "no-cache",  
+    "Connection": "keep-alive",  
+  });  
+  
+  const stream = new PassThrough();  
+  
+  ctx.status = 200;  
+  ctx.body = stream;  
+  
+  const content = "SSE是一种基于 HTTP 的服务器推送技术，它允许服务器向客户端推送数据。SSE 使用一个单向的连接来传输数据，而不是使用 HTTP 请求/响应模型。这意味着 SSE 连接一旦建立，服务器就可以向客户端推送数据，而客户端无法主动发起请求。";  
+  
+  // 将内容分割成多个事件消息  
+  const messages = content.split(''); // 每个字符作为一个消息  
+  
+  const sendMessage = () => {  
+    if (messages.length > 0) {  
+      const message = messages.shift(); // 获取并移除第一个消息  
+      stream.write(`data: ${message}\n\n`); // 发送消息，包括换行符  
+      setTimeout(sendMessage, 100); // 延迟后发送下一个消息  
+    } else {  
+      stream.end(); // 没有更多消息时结束流  
+    }  
+  };  
+  
+  sendMessage(); // 开始发送消息  
+  
+  stream.on("close", () => {  
+    console.log("已关闭");  
+  });  
+});
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
-
-router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
-  }
-})
 
 module.exports = router
--- web_sse/src/pages/home.tsx
+++ web_sse/src/pages/home.tsx
@@ -1,9 +1,28 @@
 import React from "react"
+import axios from "axios"
 
 const Home:React.FC<any> = ()=>{
-  
+  const handleClick = ()=>{
+    const eventSource = new EventSource('/api/')
+
+    // 监听消息事件
+   
+    eventSource.onmessage  = (event) => console.log(event);
+
+    eventSource.onopen = () => {
+      console.log("SSE 连接成功，状态${eventSource.readyState}<br />")
+    }
+
+    eventSource.onerror = () => {
+    console.log("SSE 连接错误，状态${eventSource.readyState}<br />")
+    eventSource.close();
+    }
+
+  }
   return (
-    <div>this is home</div>
+    <div>
+      <button onClick={()=>handleClick()}>创建连接</button>
+    </div>
   )
 }
 
